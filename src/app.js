7b4a04769f3f2f22e92a409b4b3bcc27867977e0
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-11-10 16:36:28 -0300

    feat: post /participants endpoint added, body is validated

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import Joi from "joi";
 
 const app = express();
 
@@ -14,9 +15,39 @@ dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-db = mongoClient.db("batePapoUol");
+//connect mongoClient
+try {
+  await mongoClient.connect();
+  db = mongoClient.db("chatUOL");
+} catch (error) {
+  console.log(error);
+}
 
 //code
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  const schema = Joi.object({ username: Joi.string().alphanum().required() });
+
+  const result = schema.validate(req.body);
+  if (result) {
+    res.status(422).send(result.error.message);
+    return;
+  }
+
+  try {
+    await db
+      .collection("participants")
+      .insertOne({ name, lastStatus: Date.now() });
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(409);
+    console.log(error);
+  }
+});
 
 //port
 app.listen(5000, () => console.log("Server running in port 5000"));
+
+function userExist(name) {
+  return db.collection("users").findOne(name) ? true : false;
+}
